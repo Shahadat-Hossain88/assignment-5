7839deac6902acef5bfebd7774d1af81011869aa
Shahadat Hossain <mdshahadat1802@gmail.com>
2021-02-07 12:06:02 -0600

    second one

--- hungry.js
+++ hungry.js
@@ -3,10 +3,6 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 .then(data =>  displayMenu(data.categories));
 
 
-const displayMenuDetail = name => {
-    console.log(name);
-}
-
 const displayMenu = menus =>{
     //  console.log(menus);
     const menusDiv = document.getElementById("menus");
@@ -15,56 +11,35 @@ const displayMenu = menus =>{
 
         const menuDiv = document.createElement('div');
         menuDiv.className = 'menu';
-        // menu.onclick ="displayMenuDetail('chiken')"
 
         const  menuInfo =  ` 
              <img  src='${ menu.strCategoryThumb }'>
              <p class=""> ${menu.strCategory}</p>
-         `
+             <button onclick="displayMenuDetails('${menu.strCategory}')">Details</button>
+         `;
        
         menuDiv.innerHTML = menuInfo;
         menusDiv.appendChild(menuDiv);
     });
 
     
+}
 
+const displayMenuDetails = name => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>  renderMenuInfo(data.meals[0]));
+}
+const renderMenuInfo = menu => {
+    const menuDiv = document.getElementById('menuDetail');
+    menuDiv.innerHTML = `
+    <img  src='${ menu.strMealThumb }'>
+    <h3>${menu.strMeal}</h3>
+    
 
-    // for (let i = 0; i < menus.length; i++) {
-        
-    //     const menu = menus[i];
-    //     const menuDiv = document.createElement('div');
-    //     menuDiv.className = 'menu';
-
-    //     // const picture = document.createElement('img');
-    //     // picture.src = menu.strCategoryThumb;
-
-    //     // // const id = document.createElement('h3');
-    //     // // id.innerText = menu.idCategory;
-    //     // const category = document.createElement('h1');
-    //     // category.innerText = menu.strCategory;
-    //     // // const categoryDescription = document.createElement('p');
-    //     // // categoryDescription.innerText = menu.strCategoryDescription;
-
-    //     // menuDiv.appendChild(picture);
-    //     // // menuDiv.appendChild(id);
-    //     // menuDiv.appendChild(category);
-    //     // // menuDiv.appendChild(categoryDescription);
-        
-
-    //     const menuInfo = ` 
-    //         <img  src='${ menu.strCategoryThumb }'>
-    //         <h5> ${menu.strCategory}</h5>
-    //     `
-    //     menuDiv.innerHTML = menuInfo;
-    //     menusDiv.appendChild(menuDiv);
-        
-        
-    // }
+    `;
 }
 
 
-// let searchBtn = document.getElementById("search-btn");
-// searchBtn .addEventListener('click', function () {
-//     const firstDivArea = document.getElementById("first-div");
-//     firstDivArea.style.display = 'none';
-// })
+
